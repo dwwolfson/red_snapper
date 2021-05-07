034f871bddcef46b591c92e1497081d412f72d49
David Wolfson <dwwolfson@gmail.com>
2021-05-06 22:13:55 -0500

    create additional raster

--- scripts/covariate_related/create_distance_edge_raster.R
+++ scripts/covariate_related/create_distance_edge_raster.R
@@ -5,13 +5,13 @@ library(raster)
 library(tidyverse)
 library(rgeos)
 library(fasterize)
-library(gridExtra)
+
 
 seabed<-st_read(here("data/Seabed Maps/Shapefile/NF_19_06_Segmentation.shp"))
 crs_seabed<-st_crs(seabed)
 
 
-# plot(seabed["Class_name"])
+ # plot(seabed["Class_name"])
 
 # Merge the hardbottom categories
 seabed<-seabed %>% 
@@ -37,3 +37,31 @@ dist_ras<-distance(line_ras)
 
 # save raster
 writeRaster(dist_ras, here("processed_data/dist_to_edge_raster/dist_ras.tif"))
+
+################################################
+###############################################
+# Merge sand with low hard-bottom
+seabed<-seabed %>% 
+  mutate(binary_hab=ifelse(Class_name%in%c("High", "Medium"), "med/high_hard-bottom", "low_hard-bottom/sand"))
+
+seabed<-seabed %>% 
+  group_by(binary_hab) %>% 
+  summarise(geometry=st_union(geometry))
+
+
+
+# Convert from polygon (with area), to line (w/o area)
+sea_lines<-st_cast(seabed, "MULTILINESTRING")
+
+ras<- raster(here("data/Seabed Maps/geotiff/", "ChickenRock_Classification.tif"))
+
+
+# Convert edge lines to sf polygon with a buffer and then turn to a raster
+# fasterize only works with polygons, not lines
+line_buf<-st_buffer(sea_lines, 1)
+line_ras<-fasterize(line_buf, ras)
+dist_ras<-distance(line_ras)
+
+# save raster
+writeRaster(dist_ras, here("processed_data/dist_to_edge_raster/sand_and_low_vs_med_and_high/sand-low_vs_med-high.tif"))
+
